ff9c18e71e2614aa18007401f37a965ed1c7013b
AdTekDev <nguyenngtu777@gmail.com>
2020-04-06 08:47:51 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="vi" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Cloud-Php-Ex000</title>
+  <meta charset="utf-8">
+</head>
+<body>
+
+  <h1>Cloud-Php-Ex000</h1>
+  <p>Chạy thử 1.</p>
+
+</body>
+</html>
